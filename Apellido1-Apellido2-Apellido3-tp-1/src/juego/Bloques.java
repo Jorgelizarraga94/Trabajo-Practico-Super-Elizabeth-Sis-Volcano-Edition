6295166c79d7b4bdbf356010510716c1581f4a92
Jorge Lizarraga <105560564+Jorgelizarraga94@users.noreply.github.com>
2024-05-15 22:26:08 -0300

    Add files via upload

--- Apellido1-Apellido2-Apellido3-tp-1/src/juego/Bloques.java
+++ Apellido1-Apellido2-Apellido3-tp-1/src/juego/Bloques.java
@@ -1,10 +1,12 @@
 package juego;
 
 import java.awt.Color;
+import java.util.Random;
 
 import entorno.Entorno;
 
 public class Bloques {
+	Random random;
 	int x;
 	int y;
 	int ancho;
@@ -35,9 +37,11 @@ public class Bloques {
 	}
 	public void crearPiso(Bloques bloque,Bloques [] conjuntoBloques , int y) {
 		int suma = 25;
+		this.random = new Random();
 		for (int i = 0; i < conjuntoBloques.length; i++) {
-			conjuntoBloques[i] = new Bloques(suma , y , 50, 30 ,false);
+			conjuntoBloques[i] = new Bloques(suma , y , 50, 30 , random.nextBoolean());
 			suma += 50;
+			System.out.println(conjuntoBloques[i].seRompe);
 		}
 	}
 	public void dibujar(Entorno entorno) {
