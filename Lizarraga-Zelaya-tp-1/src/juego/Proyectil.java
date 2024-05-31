94e7cba1459774a4e54dde70c50219d5542d8ca3
Jorge Lizarraga <105560564+Jorgelizarraga94@users.noreply.github.com>
2024-05-30 23:06:02 -0300

    Add files via upload

---
+++ Lizarraga-Zelaya-tp-1/src/juego/Proyectil.java
@@ -0,0 +1,79 @@
+package juego;
+
+import java.awt.Color;
+import java.awt.Image;
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Proyectil {
+	private double x;
+	private double y;
+	private int ANCHO = 10;
+	private int ALTO = 10;
+	private boolean disparoActivo = false;
+	private Image imagen;
+	//----------------Constructores-----------------//
+	public Proyectil(double x, double y, int ANCHO, int ALTO) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.ANCHO = ANCHO;
+		this.ALTO = ALTO;
+		imagen = Herramientas.cargarImagen("bolaDeFuego.png");
+	}
+	//----------------Getters y Setters-----------------//
+	public double getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getANCHO() {
+		return ANCHO;
+	}
+	
+	public void setANCHO(int ANCHO) {
+		this.ANCHO = ANCHO;
+	}
+	
+	public int getALTO() {
+		return ALTO;
+	}
+	
+	public void setALTO(int ALTO) {
+		this.ALTO = ALTO;
+	}
+	
+	public boolean getDisparoActivo() {
+		return disparoActivo;
+	}
+	
+	public void setDisparoActivo(boolean disparoActivo) {
+		this.disparoActivo = disparoActivo;
+	}
+	//----------------Metodos-----------------//
+	public void disparoDer(Entorno entorno ,int velocidadDisparo) {
+		//entorno.dibujarRectangulo(this.x, this.y, this.ANCHO, this.ALTO, 0, Color.blue);
+		double bala =  this.x += velocidadDisparo;
+		this.x = bala;
+		entorno.dibujarImagen(imagen, this.x, this.y, 0, 0.2);
+	}
+	
+	public void disparoIzq(Entorno entorno , int velocidadDisparo) {
+		//entorno.dibujarRectangulo(this.x, this.y, this.ANCHO, this.ALTO, 0, Color.red);
+		double bala =  this.x -= velocidadDisparo;
+		this.x = bala;
+		entorno.dibujarImagen(imagen, this.x, this.y, 0, 0.2);
+	}
+	
+}
